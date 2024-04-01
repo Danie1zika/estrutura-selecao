cc4a68344e4fe00989fd46c014fc689973850e1c
Danie1zika <danielbrandao03420@gmail.com>
2024-04-01 10:32:53 -0400

    questao 9

---
+++ questao9.js
@@ -0,0 +1,15 @@
+/**Questão 9: Como você pode usar uma declaração if, else if, else para 
+classificar um triângulo em equilátero, isósceles ou escaleno com base em 
+seus lados em JavaScript? 
+ */
+var lado1 = 1;
+var lado2 = 2;
+var lado3 = 3;
+
+if (lado1 === lado2 && lado2 === lado3) {
+    console.log("triangulo equilatero");
+} else if (lado2 === lado1 && lado1 !== lado3) {
+    console.log("triangulo isosceles")
+} else { (lado3 === lado1 && lado2 !== lado3);
+    console.log("triangulo escaleno")
+}
