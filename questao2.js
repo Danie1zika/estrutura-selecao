7647e33f43b373874dfb41dd485a3ca326d09bc5
Danie1zika <danielbrandao03420@gmail.com>
2024-04-01 10:32:15 -0400

    questao 2

---
+++ questao2.js
@@ -0,0 +1,12 @@
+/**Questão 2: Como você pode usar uma declaração if para verificar se uma 
+string está vazia em JavaScript?
+ */
+let dia1 = "sábado";
+let dia2 = ""
+
+if (dia === "sábado") {
+    console.log("hoje é sábado");
+
+} else { (dia2 === "")
+    console.log("está vazio")
+  }
